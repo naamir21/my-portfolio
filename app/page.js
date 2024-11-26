8b900977b64566c22b92ac57b64bbb8245e9aa74
naamir21 <nida.aamir@edu.sait.ca>
2024-11-26 03:11:58 -0700

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center' }}>
+      <div>
+        <h1>My Portfolio</h1>
+        <p>Welcome to my portfolio</p>
+      </div>
+    </div>
+  );
+}
